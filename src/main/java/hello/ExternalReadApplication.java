97e636cb653099e272941f8fb912ab5a5af99100
kdo_1999/mac <kdo_1999@naver.com>
2024-11-14 00:06:23 +0900

    feat: profile 예제 코드 추가

--- src/main/java/hello/ExternalReadApplication.java
+++ src/main/java/hello/ExternalReadApplication.java
@@ -9,12 +9,13 @@ import hello.config.MyDataSourceEnvConfig;
 import hello.config.MyDataSourcePropertiesV1;
 import hello.config.MyDataSourceValueConfig;
 import hello.datasource.MyDataSourceConfigV1;
+import hello.datasource.MyDataSourceConfigV3;
 
 // @Import(MyDataSourceEnvConfig.class)
 // @Import(MyDataSourceValueConfig.class)
-@Import(MyDataSourceConfigV1.class)
-@SpringBootApplication(scanBasePackages = "hello.datasource")
-@ConfigurationPropertiesScan({"hello"})
+// @Import(MyDataSourceConfigV1.class)
+@Import(MyDataSourceConfigV3.class)
+@SpringBootApplication(scanBasePackages = {"hello.datasource", "hello.pay"})
 public class ExternalReadApplication {
 
     public static void main(String[] args) {
